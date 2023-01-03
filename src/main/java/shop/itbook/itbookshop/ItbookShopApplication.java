81ab0c9f4ff191caa0031a4a2d6fe001e2af4b38
suy00065 <suy00065@gmail.com>
2023-01-03 11:47:49 +0900

    [feat] : 풀리퀘스트 테스트

--- src/main/java/shop/itbook/itbookshop/ItbookShopApplication.java
+++ src/main/java/shop/itbook/itbookshop/ItbookShopApplication.java
@@ -9,6 +9,7 @@ public class ItbookShopApplication {
     public static void main(String[] args) {
         System.out.println("테스트");
         System.out.println("hi2");
+        System.out.println("풀리퀘스트 테스트");
         SpringApplication.run(ItbookShopApplication.class, args);
     }
 
