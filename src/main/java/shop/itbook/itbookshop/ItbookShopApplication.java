6d9a89153b22c9a00b2f939bc98de996b7904b53
suy00065 <suy00065@gmail.com>
2023-01-03 11:19:32 +0900

    first commit

---
+++ src/main/java/shop/itbook/itbookshop/ItbookShopApplication.java
@@ -0,0 +1,13 @@
+package shop.itbook.itbookshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItbookShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItbookShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/shop/itbook/itbookshop/ItbookShopApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.itbook.itbookshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItbookShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
