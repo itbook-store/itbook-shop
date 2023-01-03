92379abd5432179de5d23ee06b1251baa47eccd5
suy00065 <suy00065@gmail.com>
2023-01-03 16:24:06 +0900

    [feat] : 젠킨스 잘 돌아가는지

--- src/main/java/shop/itbook/itbookshop/ItbookShopApplication.java
+++ src/main/java/shop/itbook/itbookshop/ItbookShopApplication.java
@@ -7,9 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ItbookShopApplication {
 
     public static void main(String[] args) {
-        System.out.println("테스트");
-        System.out.println("hi2");
-        System.out.println("풀리퀘스트 테스트");
+        System.out.println("젠킨스 테스트");
         SpringApplication.run(ItbookShopApplication.class, args);
     }
 
