f4c7d35f04fe029ad24f48facccf8ce53fb9260e
suy00065 <suy00065@gmail.com>
2023-01-04 09:18:45 +0900

    [feat] : 일부 수정

--- src/main/java/shop/itbook/itbookshop/ItbookShopApplication.java
+++ src/main/java/shop/itbook/itbookshop/ItbookShopApplication.java
@@ -3,12 +3,19 @@ package shop.itbook.itbookshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *
+ */
 @SpringBootApplication
 public class ItbookShopApplication {
 
     public static void main(String[] args) {
         System.out.println("젠킨스 테스트");
         SpringApplication.run(ItbookShopApplication.class, args);
+    }
+
+    public void test() {
+
     }
 
 }
