642416a93d517f847fd0e44ee49b7d727fffc15b
yihoney <109622@naver.com>
2023-01-05 21:39:49 +0900

    [feat] : 결제 엔티티 추가

---
+++ src/main/java/shop/itbook/itbookshop/payment/entity/Card.java
@@ -0,0 +1,51 @@
+package shop.itbook.itbookshop.payment.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * @author
+ * @since 1.0
+ */
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "card")
+@Entity
+public class Card {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "card_no", nullable = false)
+    private Long cardNo;
+
+    @Column(name = "total_amount", nullable = false)
+    private Long totalAmount;
+
+    @Column(name = "issuer_code", nullable = false, columnDefinition = "varchar(20)")
+    private String issuerCode;
+
+    @Column(name = "acquire_code", nullable = false, columnDefinition = "varchar(20)")
+    private String acquireCode;
+
+    @Column(name = "approve_number", nullable = false)
+    private Integer installmentPlanMonths;
+
+    @Column(name = "approve_number", nullable = false, columnDefinition = "varchar(20)")
+    private String approveNumber;
+
+    @Column(name = "is_use_card_point")
+    private boolean isUseCardPoint;
+
+}
--- src/main/java/shop/itbook/itbookshop/payment/entity/PaymentStatus.java
+++ src/main/java/shop/itbook/itbookshop/payment/entity/PaymentStatus.java
@@ -1,36 +1,9 @@
-package shop.itbook.itbookshop.payment.entity;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+package shop.itbook.itbookshop.product.entity;
 
 /**
- * 결제상태에 대한 엔티티입니다.
- *
- * @author 이하늬
+ * @author
  * @since 1.0
  */
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@Table(name = "payment_status")
-@Entity
 public class PaymentStatus {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "payment_status_no", nullable = false)
-    private Integer paymentStatusNo;
-
-    @Column(name = "name", nullable = false, columnDefinition = "varchar(20)")
-    private String name;
 
 }
