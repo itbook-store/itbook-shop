0d7cf6fd4622481ca93e00cfd9db619bff79118e
jun1718 <gbeovhsqhtka@naver.com>
2023-01-05 22:06:54 +0900

    [refact] : 카테고리 joincolumn 변경

--- src/main/java/shop/itbook/itbookshop/categori/entity/Category.java
+++ src/main/java/shop/itbook/itbookshop/categori/entity/Category.java
@@ -6,6 +6,7 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -33,7 +34,7 @@ public class Category {
     private Integer categoryNo;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @Column(name = "parent_category_no")
+    @JoinColumn(name = "parent_category_no")
     private Category parentCategoryNo;
 
     @Column(name = "category_name")
