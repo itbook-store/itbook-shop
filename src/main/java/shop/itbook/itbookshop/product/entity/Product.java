241f76004d266c18a8e1908f2a8abd297adb02f7
suy00065 <suy00065@gmail.com>
2023-01-05 17:27:38 +0900

    [feat] 상품 엔티티 작성

---
+++ src/main/java/shop/itbook/itbookshop/product/entity/Product.java
@@ -0,0 +1,72 @@
+package shop.itbook.itbookshop.product.entity;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+/**
+ * @author
+ * @since 1.0
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Product {
+    @Id
+    @Column(name = "product_no", nullable = false)
+    private Long productNo;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "simple_description", nullable = false)
+    private String simpleDescription;
+
+    @Column(name = "details_description")
+    private String detailsDescription;
+
+    @Column(name = "stock", nullable = false)
+    @ColumnDefault("0")
+    private Integer stock;
+
+    @Column(name = "product_created_at", nullable = false)
+    private LocalDateTime productCreatedAt;
+
+    @Column(name = "is_selled", nullable = false)
+    private Boolean isSelled;
+
+    @Column(name = "is_deleted", nullable = false)
+    private Boolean isDeleted;
+
+    @Column(name = "is_subscription", nullable = false)
+    private Boolean isSubscription;
+
+    @Column(name = "thumnail_url", nullable = false)
+    private String thumnailUrl;
+
+    @Column(name = "daily_hits", nullable = false)
+    @ColumnDefault("0")
+    private Long dailyHits;
+
+    @Column(name = "fixed_price", nullable = false)
+    private Long fixedPrice;
+
+    @Column(name = "point_saving_percent", nullable = false)
+    @ColumnDefault("0")
+    private Integer pointSavingPercent;
+
+    @Column(name = "discount_percent", nullable = false)
+    @ColumnDefault("0")
+    private Integer discountPercent;
+
+    @Column(name = "raw_price", nullable = false)
+    private Long rawPrice;
+}
