7868acc26e81801846c85e35a88ef0cf72a6a5fe
HeLLo2 <popt0@naver.com>
2023-01-06 15:21:25 +0900

    [refactor] : 상품 문의 text 타입 columnDefinition 추가.
    content 변수가 mysql 의 text 타입이여서 컬럼 속성에 columnDefinition = "text" 추가.

--- src/main/java/shop/itbook/itbookshop/productinquiry/entity/ProductInquiry.java
+++ src/main/java/shop/itbook/itbookshop/productinquiry/entity/ProductInquiry.java
@@ -47,7 +47,7 @@ public class ProductInquiry {
     @Column(name = "title", nullable = false, columnDefinition = "varchar(20)")
     private String title;
 
-    @Column(name = "content", nullable = false)
+    @Column(name = "content", nullable = false, columnDefinition = "text")
     private String content;
 
     @Column(name = "is_pubilc", nullable = false)
