236d8473b71c1354ef3d0a9ae7e9710e13b4085e
gwanii <lung4536@gmail.com>
2023-01-06 15:10:25 +0900

    [feat] : 상품에 대한 관계테이블 && 문의 댓글 테이블 등 총 4개 추가

---
+++ src/main/java/shop/itbook/itbookshop/memberrecentlyviewedproduct/entity/MemberRecentlyViewedProduct.java
@@ -0,0 +1,63 @@
+package shop.itbook.itbookshop.memberrecentlyviewedproduct.entity;
+
+import java.io.Serializable;
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import shop.itbook.itbookshop.member.entity.Member;
+import shop.itbook.itbookshop.memberrole.entity.MemberRole;
+import shop.itbook.itbookshop.recentlyviewedproduct.entity.RecentlyViewedProduct;
+
+/**
+ * 회원 최근 본 상품 관계 테이블에 대한 엔티티 입니다.
+ *
+ * @author 강명관
+ * @since 1.0
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "member_recently_viewed_product")
+public class MemberRecentlyViewedProduct {
+
+    @EmbeddedId
+    private Pk pk;
+
+    @MapsId("memberNo")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_no")
+    private Member member;
+
+    @MapsId("recentlyViewedProductNo")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "recentlyViewedProductNo")
+    private RecentlyViewedProduct recentlyViewedProduct;
+
+    /**
+     * The type Pk. 회원과 최근본 상품 복합키를 주키로 하기위한 클래스 입니다.
+     */
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @EqualsAndHashCode
+    @Embeddable
+    public static class Pk implements Serializable {
+
+        private Long memberNo;
+
+        private Long recentlyViewedProductNo;
+
+    }
+}
---
+++ src/main/java/shop/itbook/itbookshop/producttyperegistration/entity/ProductTypeRegistration.java
@@ -0,0 +1,63 @@
+package shop.itbook.itbookshop.producttyperegistration.entity;
+
+import java.io.Serializable;
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import shop.itbook.itbookshop.product.entity.Product;
+import shop.itbook.itbookshop.producttype.entity.ProductType;
+
+/**
+ * 상품 유형 등록 테이블에 대한 엔티티 입니다.
+ *
+ * @author 강명관
+ * @since 1.0
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "product_type_registration")
+public class ProductTypeRegistration {
+
+    @EmbeddedId
+    private Pk pk;
+
+    @MapsId("productNo")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_no")
+    private Product product;
+
+    @MapsId("productTypeNo")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_type_no")
+    private ProductType productType;
+
+
+    /**
+     * The type Pk. 상품과 상품타입 복합키를 주키로 하기위한 클래스 입니다.
+     */
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @EqualsAndHashCode
+    @Embeddable
+    public static class Pk implements Serializable {
+
+        private Long productNo;
+
+        private Integer productTypeNo;
+
+    }
+}
