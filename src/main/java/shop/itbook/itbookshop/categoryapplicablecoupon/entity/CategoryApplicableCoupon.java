5af7edbfaacf5f487d7e2d58500a686bd0ce2f6f
dahye <sdh46@chosun.kr>
2023-01-06 15:10:44 +0900

    [feat] : 카테고리 적용 쿠폰 엔터티 추가

---
+++ src/main/java/shop/itbook/itbookshop/categoryapplicablecoupon/entity/CategoryApplicableCoupon.java
@@ -0,0 +1,61 @@
+package shop.itbook.itbookshop.categoryapplicablecoupon.entity;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import shop.itbook.itbookshop.coupon.entity.Coupon;
+import shop.itbook.itbookshop.orderproduct.entity.OrderProduct;
+
+/**
+ * 카테고리쿠폰적용 관련 쿠폰입니다.
+ *
+ * @author 송다혜
+ * @since 1.0
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "category_applicable_coupon")
+public class CategoryApplicableCoupon {
+
+    @EmbeddedId
+    private Pk pk;
+
+    @MapsId("orderProductNo")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_product_no")
+    private OrderProduct orderProduct;
+
+    @MapsId("couponNo")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "coupon_no")
+    private Coupon coupon;
+
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @EqualsAndHashCode
+    @Embeddable
+    public static class Pk implements Serializable {
+        @Column(name = "order_product_no")
+        private Long orderProductNo;
+
+        @Column(name = "coupon_no")
+        private Integer couponNo;
+    }
+}
