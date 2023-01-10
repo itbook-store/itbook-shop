2e3712921e7afe4c0e0ef36b969b1fbd00f207d3
jun1718 <gbeovhsqhtka@naver.com>
2023-01-10 16:29:34 +0900

    [feat] : 수정 및 삭제 controller 추가

--- src/main/java/shop/itbook/itbookshop/category/controller/adminapi/CategoryAdminController.java
+++ src/main/java/shop/itbook/itbookshop/category/controller/adminapi/CategoryAdminController.java
@@ -1,22 +1,28 @@
 package shop.itbook.itbookshop.category.controller.adminapi;
 
-import com.querydsl.core.types.dsl.BooleanExpression;
+import io.lettuce.core.dynamic.annotation.Param;
 import java.util.List;
+import java.util.Objects;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import shop.itbook.itbookshop.category.dto.request.CategoryRequestDto;
+import shop.itbook.itbookshop.category.dto.response.CategoryAllFieldResponseDto;
 import shop.itbook.itbookshop.category.dto.response.CategoryChildResponseProjectionDto;
 import shop.itbook.itbookshop.category.dto.response.CategoryNoResponseDto;
 import shop.itbook.itbookshop.category.dto.response.CategoryResponseDto;
 import shop.itbook.itbookshop.category.dto.response.CategoryResponseProjectionDto;
+import shop.itbook.itbookshop.category.dto.response.CategoryWithoutParentFieldResponseDto;
 import shop.itbook.itbookshop.category.resultmessageenum.CategoryResultMessageEnum;
 import shop.itbook.itbookshop.category.service.adminapi.CategoryAdminService;
 import shop.itbook.itbookshop.common.response.CommonResponseBody;
@@ -33,6 +39,7 @@ import shop.itbook.itbookshop.common.response.CommonResponseBody;
 public class CategoryAdminController {
 
     private final CategoryAdminService categoryAdminService;
+    private final static Boolean SUCCESSED = Boolean.TRUE;
 
     /**
      * 카테고리 저장 요청을 처리하는 메서드입니다.
@@ -49,7 +56,7 @@ public class CategoryAdminController {
             new CategoryNoResponseDto(categoryAdminService.addCategory(categoryRequestDto));
 
         CommonResponseBody<CategoryNoResponseDto> commonResponseBody = new CommonResponseBody<>(
-            new CommonResponseBody.CommonHeader(true, HttpStatus.CREATED.value(),
+            new CommonResponseBody.CommonHeader(SUCCESSED, HttpStatus.CREATED.value(),
                 CategoryResultMessageEnum.CATEGORY_SAVE_SUCCESS_MESSAGE.getSuccessMessage()),
             categoryNoResponseDto);
 
@@ -63,13 +70,14 @@ public class CategoryAdminController {
      * @author 최겸준
      */
     @GetMapping
-    public ResponseEntity<CommonResponseBody<List<CategoryResponseProjectionDto>>> categoryList() {
+    public ResponseEntity<CommonResponseBody<List<CategoryAllFieldResponseDto>>> categoryList(
+        @RequestParam(value = "isHidden", required = false) Boolean isHidden) {
 
-        CommonResponseBody<List<CategoryResponseProjectionDto>> commonResponseBody =
+        CommonResponseBody<List<CategoryAllFieldResponseDto>> commonResponseBody =
             new CommonResponseBody<>(
-                new CommonResponseBody.CommonHeader(true, HttpStatus.OK.value(),
+                new CommonResponseBody.CommonHeader(SUCCESSED, HttpStatus.OK.value(),
                     CategoryResultMessageEnum.CATEGORY_LIST_SUCCESS_MESSAGE.getSuccessMessage()),
-                categoryAdminService.findCategoryList());
+                categoryAdminService.findCategoryList(isHidden));
 
         return ResponseEntity.ok().body(commonResponseBody);
     }
@@ -84,11 +92,11 @@ public class CategoryAdminController {
      * @author 최겸준
      */
     @GetMapping("/{categoryNo}/child-categories")
-    public ResponseEntity<CommonResponseBody<List<CategoryChildResponseProjectionDto>>>
+    public ResponseEntity<CommonResponseBody<List<CategoryWithoutParentFieldResponseDto>>>
     categoryChildList(@PathVariable Integer categoryNo) {
 
         return ResponseEntity.ok().body(new CommonResponseBody<>(
-            new CommonResponseBody.CommonHeader(true, HttpStatus.OK.value(),
+            new CommonResponseBody.CommonHeader(SUCCESSED, HttpStatus.OK.value(),
                 CategoryResultMessageEnum.CATEGORY_CHILD_LIST_SUCCESS_MESSAGE.getSuccessMessage()),
             categoryAdminService.findCategoryChildList(categoryNo)));
     }
@@ -105,10 +113,54 @@ public class CategoryAdminController {
         @PathVariable Integer categoryNo) {
 
         CommonResponseBody<CategoryResponseDto> commonResponseBody = new CommonResponseBody<>(
-            new CommonResponseBody.CommonHeader(true, HttpStatus.OK.value(),
+            new CommonResponseBody.CommonHeader(SUCCESSED, HttpStatus.OK.value(),
                 CategoryResultMessageEnum.CATEGORY_DETAILS_SUCCESS_MESSAGE.getSuccessMessage()),
             categoryAdminService.findCategoryResponseDtoThroughCategoryNo(categoryNo));
 
         return ResponseEntity.ok().body(commonResponseBody);
+    }
+
+    /**
+     * 카테고리 수정요청시 요청을 받고 반환하는 역할을 담당합니다.
+     *
+     * @param categoryNo 수정해야할 카테고리 번호입니다.
+     * @return 성공여부 및 기본적인 반환정보를 클라이언트에게 반환합니다. 성공시 204
+     * @author 최겸준
+     */
+    @PutMapping("/{categoryNo}")
+    public ResponseEntity<CommonResponseBody<Void>> categoryModify(
+        @PathVariable Integer categoryNo, @RequestBody CategoryRequestDto categoryRequestDto) {
+
+        categoryAdminService.modifyCategory(categoryNo, categoryRequestDto);
+
+        CommonResponseBody<Void> commonResponseBody =
+            new CommonResponseBody<>(new CommonResponseBody.CommonHeader(
+                SUCCESSED, HttpStatus.NO_CONTENT.value(),
+                CategoryResultMessageEnum.CATEGORY_MODIFY_SUCCESS_MESSAGE.getSuccessMessage()),
+                null);
+
+        return ResponseEntity.status(HttpStatus.OK.value()).body(commonResponseBody);
+    }
+
+    /**
+     * 카테고리 삭제요청을 처리하는 기능입니다.
+     *
+     * @param categoryNo 삭제 해야하는 카테고리의 번호입니다.
+     * @return 카테고리 삭제여부 등의 갑을 넣어 클라이언트에게 반환합니다.
+     * @author 최겸준
+     */
+    @DeleteMapping("/{categoryNo}")
+    public ResponseEntity<CommonResponseBody<Void>> categoryRemove(
+        @PathVariable Integer categoryNo) {
+
+        categoryAdminService.removeCategory(categoryNo);
+
+        CommonResponseBody<Void> commonResponseBody =
+            new CommonResponseBody<>(
+                new CommonResponseBody.CommonHeader(SUCCESSED, HttpStatus.OK.value(),
+                    CategoryResultMessageEnum.CATEGORY_REMOVE_SUCCESS.getSuccessMessage()), null);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT.value())
+            .body(commonResponseBody);
     }
 }
