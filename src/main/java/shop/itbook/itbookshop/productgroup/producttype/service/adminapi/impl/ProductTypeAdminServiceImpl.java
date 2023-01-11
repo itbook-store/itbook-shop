f8ad21230972a1d5d0c7930eb71474f80c0dd751
yihoney <109622@naver.com>
2023-01-11 21:29:43 +0900

    [docs] : javadoc 수정

---
+++ src/main/java/shop/itbook/itbookshop/productgroup/producttype/service/adminapi/impl/ProductTypeAdminServiceImpl.java
@@ -0,0 +1,32 @@
+package shop.itbook.itbookshop.productgroup.producttype.service.adminapi.impl;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import shop.itbook.itbookshop.productgroup.producttype.entity.ProductType;
+import shop.itbook.itbookshop.productgroup.producttype.repository.ProductTypeRepository;
+import shop.itbook.itbookshop.productgroup.producttype.service.adminapi.ProductTypeAdminService;
+
+/**
+ * ProductTypeAdminService 인터페이스를 구현한 상품 Service 클래스입니다.
+ *
+ * @author 이하늬
+ * @since 1.0
+ */
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ProductTypeAdminServiceImpl implements ProductTypeAdminService {
+
+    private final ProductTypeRepository productTypeRepository;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<ProductType> findProductTypeList() {
+        return productTypeRepository.findAll();
+
+    }
+}
