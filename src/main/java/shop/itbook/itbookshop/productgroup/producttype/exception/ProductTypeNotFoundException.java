7a82d3733acc21e0148e3765d5ba419231f62085
yihoney <109622@naver.com>
2023-01-12 21:06:21 +0900

    [feat] : productType 단건 조회 기능 추가

--- src/main/java/shop/itbook/itbookshop/productgroup/producttype/exception/ProductTypeNotFoundException.java
+++ src/main/java/shop/itbook/itbookshop/productgroup/producttype/exception/ProductTypeNotFoundException.java
@@ -1,16 +1,16 @@
-package shop.itbook.itbookshop.productgroup.product.exception;
+package shop.itbook.itbookshop.productgroup.producttype.exception;
 
 /**
- * 상품이 존재하지 않을 때 발생시킬 예외 클래스입니다.
+ * 상품유형이 존재하지 않을 때 발생시킬 예외 클래스입니다.
  *
  * @author 이하늬
  * @since 1.0
  */
-public class ProductNotFoundException extends RuntimeException {
+public class ProductTypeNotFoundException extends RuntimeException {
 
-    public static final String MESSAGE = "존재하지 않는 상품입니다.";
+    public static final String MESSAGE = "존재하지 않는 상품유형입니다.";
 
-    public ProductNotFoundException() {
+    public ProductTypeNotFoundException() {
         super(MESSAGE);
     }
 }
