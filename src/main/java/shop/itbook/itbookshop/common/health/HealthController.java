ba5070d9f6309b0f2a7e6208c113b5beafe99134
HeLLo2 <popt0@naver.com>
2023-01-17 21:22:40 +0900

    [chore] : 헬스 체크 로직 수정2

--- src/main/java/shop/itbook/itbookshop/common/health/HealthController.java
+++ src/main/java/shop/itbook/itbookshop/common/health/HealthController.java
@@ -1,9 +1,11 @@
 package shop.itbook.itbookshop.common.health;
 
+import java.util.Objects;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -15,21 +17,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthController {
 
-    private Boolean isHealth = Boolean.TRUE;
+    private Boolean isOk = Boolean.TRUE;
+    private static final Boolean OK = Boolean.TRUE;
+    private static final Boolean INTERNAL = Boolean.FALSE;
 
     @GetMapping("/monitor/l7check")
     public ResponseEntity<Void> health() {
 
-        if (Boolean.TRUE.equals(isHealth)) {
+        if (Boolean.TRUE.equals(isOk)) {
             return ResponseEntity.ok().build();
         }
 
         return ResponseEntity.internalServerError().build();
     }
 
-    @PutMapping("/monitor/l7check")
-    public ResponseEntity<Void> healthStatusModify() {
-        isHealth = !isHealth;
-        return ResponseEntity.ok().build();
+    @PutMapping(value = "/monitor/l7check", produces = MediaType.TEXT_PLAIN_VALUE)
+    public ResponseEntity<String> healthStatusModify(@RequestParam String healthStatus) {
+
+        if (Objects.isNull(healthStatus)) {
+            this.isOk = OK;
+            return ResponseEntity.ok("now server status is OK 200");
+        }
+
+        this.isOk = INTERNAL;
+        return ResponseEntity.ok("now server status is INTERNAL 500");
     }
 }
