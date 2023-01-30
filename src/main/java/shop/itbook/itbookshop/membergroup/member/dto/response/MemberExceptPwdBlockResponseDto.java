41d9cc73ac85cdcf1dcd880a7600a739b863a8bf
suy00065 <suy00065@gmail.com>
2023-01-30 14:43:27 +0900

    [feat] : 차단회원 정보 받아올 dto 작성

--- src/main/java/shop/itbook/itbookshop/membergroup/member/dto/response/MemberExceptPwdBlockResponseDto.java
+++ src/main/java/shop/itbook/itbookshop/membergroup/member/dto/response/MemberExceptPwdBlockResponseDto.java
@@ -1,7 +1,34 @@
-package shop.itbook.itbookshop.membergroup.member.dto.response;/**
- * 
- * 
+package shop.itbook.itbookshop.membergroup.member.dto.response;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 관리자 api 에서 차단 멤버를 조회할 때 정보를 받아올 dto 입니다.
+ *
  * @author 노수연
  * @since 1.0
- */public class MemberExceptPwdBlockResponseDto {
+ */
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberExceptPwdBlockResponseDto {
+
+    private Long memberNo;
+    private String memberId;
+    private String membershipGrade;
+    private String memberStatusName;
+    private String nickname;
+    private String name;
+    private Boolean isMan;
+    private LocalDateTime birth;
+    private String phoneNumber;
+    private String email;
+    private LocalDateTime memberCreatedAt;
+    private String statusChangedReason;
+    private LocalDateTime memberStatusHistoryCreatedAt;
 }
