aaf2e540b872e00b8a812c732bde725a14d199d0
HeLLo2 <popt0@naver.com>
2023-01-31 13:46:24 +0900

    [feat] : 배송 서버 전달 예외 처리

---
+++ src/test/java/shop/itbook/itbookshop/deliverygroup/deliverystatus/repository/DeliveryStatusRepositoryTest.java
@@ -0,0 +1,24 @@
+package shop.itbook.itbookshop.deliverygroup.deliverystatus.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+/**
+ * @author 정재원
+ * @since 1.0
+ */
+@DataJpaTest
+class DeliveryStatusRepositoryTest {
+
+    @Autowired
+    DeliveryStatusRepository deliveryStatusRepository;
+
+    @Test
+    @DisplayName("Enum 을 이용해서 배송 상태 테이블에서 배송 상태 엔티티 정보 가져오기 성공")
+    void findByDeliveryStatusEnumSuccessTest() {
+    }
+}
