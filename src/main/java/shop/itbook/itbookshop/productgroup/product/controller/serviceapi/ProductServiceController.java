cdfc0e790e95c4ae97c4e41de3a0a88318c2991e
yihoney <109622@naver.com>
2023-02-02 17:04:36 +0900

    [feat] : 신간, 할인 상품유형 기능 추가

--- src/main/java/shop/itbook/itbookshop/productgroup/product/controller/serviceapi/ProductServiceController.java
+++ src/main/java/shop/itbook/itbookshop/productgroup/product/controller/serviceapi/ProductServiceController.java
@@ -44,7 +44,8 @@ public class ProductServiceController {
     public ResponseEntity<CommonResponseBody<PageResponse<ProductDetailsResponseDto>>> productList(
         @PageableDefault Pageable pageable) {
 
-        Page<ProductDetailsResponseDto> productList = productService.findProductListUser(pageable);
+        Page<ProductDetailsResponseDto> productList =
+            productService.findProductList(pageable, false);
 
         CommonResponseBody<PageResponse<ProductDetailsResponseDto>> commonResponseBody =
             new CommonResponseBody<>(
@@ -69,6 +70,30 @@ public class ProductServiceController {
 
         Page<ProductDetailsResponseDto> productList =
             productCategoryService.findProductList(pageable, categoryNo);
+
+        CommonResponseBody<PageResponse<ProductDetailsResponseDto>> commonResponseBody =
+            new CommonResponseBody<>(
+                new CommonResponseBody.CommonHeader(
+                    ProductCategoryResultMessageEnum.GET_SUCCESS.getMessage()),
+                new PageResponse<>(productList));
+
+        return ResponseEntity.status(HttpStatus.OK).body(commonResponseBody);
+    }
+
+    /**
+     * 상품유형별로 상품 조회를 요청하는 메서드입니다.
+     * 쿼리스트링으로 상품유형 번호가 파라미터로 들어올 시, 해당 상품유형 번호의 상품들을 조회합니다.
+     *
+     * @param productTypeNo 조회할 상품유형 번호입니다.
+     * @return 상품유형 번호에 해당하는 상품 리스트를 response entity에 담아 반환합니다.
+     * @author 이하늬
+     */
+    @GetMapping(params = "productTypeNo")
+    public ResponseEntity<CommonResponseBody<PageResponse<ProductDetailsResponseDto>>> productListFilteredByProductTypeNo(
+        @PageableDefault Pageable pageable, @RequestParam Integer productTypeNo) {
+
+        Page<ProductDetailsResponseDto> productList =
+            productService.findProductListByProductTypeNo(pageable, productTypeNo, false);
 
         CommonResponseBody<PageResponse<ProductDetailsResponseDto>> commonResponseBody =
             new CommonResponseBody<>(
--- src/main/java/shop/itbook/itbookshop/productgroup/product/repository/ProductRepository.java
+++ src/main/java/shop/itbook/itbookshop/productgroup/product/repository/ProductRepository.java
@@ -1,8 +1,11 @@
 package shop.itbook.itbookshop.productgroup.product.repository;
 
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import shop.itbook.itbookshop.productgroup.product.dto.response.ProductDetailsResponseDto;
 import shop.itbook.itbookshop.productgroup.product.entity.Product;
 
 /**
