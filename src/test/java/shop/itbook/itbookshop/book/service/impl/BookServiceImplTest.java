ccff81658bbc6acafc28ecc5fa5296ae3f726364
yihoney <109622@naver.com>
2023-02-06 00:26:54 +0900

    [test] : 상품 도서 관련 테스트 수정 완료

---
+++ src/test/java/shop/itbook/itbookshop/book/service/impl/BookServiceImplTest.java
@@ -0,0 +1,156 @@
+package shop.itbook.itbookshop.book.service.impl;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.mock;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+import shop.itbook.itbookshop.book.entity.Book;
+import shop.itbook.itbookshop.book.exception.BookNotFoundException;
+import shop.itbook.itbookshop.book.repository.BookRepository;
+import shop.itbook.itbookshop.book.service.BookService;
+import shop.itbook.itbookshop.book.transfer.BookTransfer;
+import shop.itbook.itbookshop.productgroup.product.dto.request.ProductBookRequestDto;
+import shop.itbook.itbookshop.productgroup.product.dto.request.ProductRequestDto;
+import shop.itbook.itbookshop.productgroup.product.dto.response.ProductDetailsResponseDto;
+import shop.itbook.itbookshop.productgroup.product.dummy.ProductBookRequestDummy;
+import shop.itbook.itbookshop.productgroup.product.entity.Product;
+import shop.itbook.itbookshop.fileservice.FileService;
+import shop.itbook.itbookshop.productgroup.product.service.AladinApiService;
+import shop.itbook.itbookshop.productgroup.product.service.ProductService;
+import shop.itbook.itbookshop.productgroup.product.transfer.ProductTransfer;
+import shop.itbook.itbookshop.productgroup.producttyperegistration.service.ProductTypeRegistrationService;
+
+/**
+ * @author 이하늬
+ * @since 1.0
+ */
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = BookServiceImpl.class)
+class BookServiceImplTest {
+
+    @Autowired
+    BookService bookService;
+
+    @MockBean
+    ProductService mockProductService;
+
+    @MockBean
+    AladinApiService mockAladinApiService;
+
+    @MockBean
+    FileService mockFileService;
+
+
+    @MockBean
+    BookRepository mockBookRepository;
+
+
+    ProductBookRequestDto productBookRequestDto;
+    ProductBookRequestDto modifyProductBookRequestDto;
+
+    MockMultipartFile mockImageFile;
+    MockMultipartFile mockPdfFile;
+
+    @BeforeEach
+    void setUp() throws IOException {
+        String imageContentType = "image/png";
+        String pdfContentType = "application/pdf";
+        String path = "src/test/resources/";
+
+        mockImageFile = new MockMultipartFile("image", "test.png", imageContentType,
+            new FileInputStream(path + "test.png"));
+        mockPdfFile = new MockMultipartFile("pdf", "test.pdf", pdfContentType,
+            new FileInputStream(path + "test.pdf"));
+
+        productBookRequestDto = ProductBookRequestDummy.getProductBookRequest();
+        modifyProductBookRequestDto = ProductBookRequestDummy.getProductBookRequest();
+        ReflectionTestUtils.setField(modifyProductBookRequestDto, "productName", "객체지향의 거짓과 오해");
+    }
+
+    @Test
+    @DisplayName("도서 등록 테스트")
+    void addBookTest_success() {
+        Product product = ProductTransfer.dtoToEntityAdd(productBookRequestDto);
+        Book book = BookTransfer.dtoToEntityAdd(productBookRequestDto, product.getProductNo());
+        given(mockProductService.addProduct(
+            mockProductService.toProductRequestDto(productBookRequestDto), mockImageFile))
+            .willReturn(product.getProductNo());
+        given(mockBookRepository.save(any(Book.class)))
+            .willReturn(book);
+
+        Long actual = bookService.addBook(productBookRequestDto, mockImageFile, mockPdfFile);
+
+        Assertions.assertThat(actual).isEqualTo(product.getProductNo());
+    }
+
+    @Test
+    @DisplayName("도서 수정 테스트")
+    void modifyProductTest_success() {
+        Product product = mock(Product.class);
+        Book book = mock(Book.class);
+
+        given(mockBookRepository.findById(anyLong()))
+            .willReturn(Optional.of(book));
+
+        bookService.modifyBook(product.getProductNo(), modifyProductBookRequestDto, mockPdfFile);
+
+        then(mockBookRepository).should().findById(anyLong());
+        then(book).should().setIsbn(anyString());
+        then(book).should().setPageCount(anyInt());
+        then(book).should().setIsEbook(anyBoolean());
+        then(book).should().setBookCreatedAt(any(LocalDateTime.class));
+    }
+
+
+    @Test
+    @DisplayName("도서 단건 조회 시 실패 테스트 - 도서가 없을 시 예외 발생")
+    void findBookTest_failure() {
+        given(mockBookRepository.findById(anyLong())).willReturn(Optional.empty());
+        Assertions.assertThatThrownBy(() -> bookService.findBook(1L))
+            .isInstanceOf(BookNotFoundException.class)
+            .hasMessage(BookNotFoundException.MESSAGE);
+    }
+
+    @Test
+    @DisplayName("도서 엔티티 단건 조회 테스트")
+    void findBookEntityTest_success() {
+
+        Product product = ProductTransfer.dtoToEntityAdd(productBookRequestDto);
+        Book book = BookTransfer.dtoToEntityAdd(productBookRequestDto, product.getProductNo());
+        given(mockProductService.addProduct(
+            mockProductService.toProductRequestDto(productBookRequestDto), mockImageFile))
+            .willReturn(product.getProductNo());
+        given(mockBookRepository.findById(anyLong())).willReturn(Optional.of(book));
+
+        Optional<Book> actualBook = mockBookRepository.findById(1L);
+
+        Assertions.assertThat(actualBook).isPresent();
+        Assertions.assertThat(actualBook.get().getProductNo())
+            .isEqualTo(product.getProductNo());
+    }
+
+
+}
--- src/test/java/shop/itbook/itbookshop/productgroup/product/service/AladinApiServiceTest.java
+++ src/test/java/shop/itbook/itbookshop/productgroup/product/service/AladinApiServiceTest.java
@@ -1,6 +1,5 @@
 package shop.itbook.itbookshop.productgroup.product.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -8,7 +7,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.web.client.RestTemplate;
+import shop.itbook.itbookshop.productgroup.product.dto.response.BookResponse;
+import shop.itbook.itbookshop.productgroup.product.dto.response.Item;
 
 /**
  * @author 이하늬
@@ -16,20 +16,36 @@ import org.springframework.web.client.RestTemplate;
  */
 @Slf4j
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = KakaoApiService.class)
+@ContextConfiguration(classes = AladinApiService.class)
 class AladinApiServiceTest {
 
     @Autowired
-    KakaoApiService kakaoApiService;
+    AladinApiService aladinApiService;
 
     @Test
-    void responseBookTest() throws JsonProcessingException {
-        KakaoApiService.BookResponse bookDetails =
-            kakaoApiService.getBookDetails("9788960777330");
+    void responseBookTest_success() {
+        Item bookDetails =
+            aladinApiService.getBookDetails("9788998139766");
 
-        log.info(bookDetails.getItem().toString());
+        log.info(bookDetails.toString());
         Assertions.assertThat(bookDetails).isNotNull();
-        Assertions.assertThat(bookDetails.getItem().get(0).getAuthor()).isEqualTo("김영한 (지은이)");
+        Assertions.assertThat(bookDetails.getAuthor()).isEqualTo("조영호 (지은이)");
+        Assertions.assertThat(bookDetails.getPublisher()).isEqualTo("위키북스");
+        Assertions.assertThat(bookDetails.getPubDate()).isEqualTo("2015-06-17");
+        Assertions.assertThat(bookDetails.getPriceStandard()).isEqualTo(20000L);
+        Assertions.assertThat(bookDetails.getTitle())
+            .isEqualTo("객체지향의 사실과 오해 - 역할, 책임, 협력 관점에서 본 객체지향");
+        Assertions.assertThat(bookDetails.getDescription())
+            .isEqualTo(
+                "위키북스 IT Leaders 시리즈 23권. 객체지향이란 무엇인가? 이 책은 이 질문에 대한 답을 찾기 위해 노력하고 있는 모든 개발자를 위한 책이다.");
+    }
+
+    @Test
+    void responseBookTest_failure() {
+        Item bookDetails =
+            aladinApiService.getBookDetails("97889981397666");
+
+        Assertions.assertThat(bookDetails).isNull();
     }
 
 }
