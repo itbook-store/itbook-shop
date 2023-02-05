098e2a5ce480676e6737a64fe9030bc63972d03e
jun1718 <gbeovhsqhtka@naver.com>
2023-02-05 12:58:01 +0900

    [test] : 포인트 쿠폰 추가 기능 repo 테스트

--- src/test/java/shop/itbook/itbookshop/pointgroup/pointhistorychild/order/repository/OrderIncreaseDecreasePointHistoryRepositoryTest.java
+++ src/test/java/shop/itbook/itbookshop/pointgroup/pointhistorychild/order/repository/OrderIncreaseDecreasePointHistoryRepositoryTest.java
@@ -1,9 +1,16 @@
+package shop.itbook.itbookshop.pointgroup.pointhistorychild.order.repository;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+
 /**
- * 
- * 
  * @author 최겸준
  * @since 1.0
- */   class OrderIncreaseDecreasePointHistoryRepositoryTest {
-  
+ */
+class OrderIncreaseDecreasePointHistoryRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
 }
