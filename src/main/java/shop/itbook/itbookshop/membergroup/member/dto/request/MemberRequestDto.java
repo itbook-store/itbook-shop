2be5b8d27140f69c9547e1e7de85f9a62823266a
suy00065 <suy00065@gmail.com>
2023-02-07 13:36:59 +0900

    [refactor] : 수정

--- src/main/java/shop/itbook/itbookshop/membergroup/member/dto/request/MemberRequestDto.java
+++ src/main/java/shop/itbook/itbookshop/membergroup/member/dto/request/MemberRequestDto.java
@@ -45,11 +45,8 @@ public class MemberRequestDto {
     private LocalDateTime birth;
 
     @NotBlank(message = "비밀번호는 null값 및 공백을 허용하지 않습니다.")
-    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,20}",
-        message = "비밀번호는 영문 대소문자와 숫자, 특수기호가 적어도 1개 이상씩 포함되어야하고 길이는 8자 ~ 255자의 비밀번호여야 합니다.")
     private String password;
 
-    @Pattern(regexp = "^[0-9]{11}$", message = "전화번호 형식에 맞춰 입력해주세요. 숫자만 입력할 수 있습니다.")
     @NotBlank(message = "핸드폰 번호는 null값 및 공백을 허용하지 않습니다.")
     private String phoneNumber;
 
