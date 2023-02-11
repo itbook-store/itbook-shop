7f5cb9b077994219e4734a54c93e50bdb53cb1b1
HeLLo2 <popt0@naver.com>
2023-02-12 05:06:20 +0900

    [feat] : 재 주문 로직 추가.
    결제 완료하지 않고 종료한 결제 대기 상태의 주문 엔티티를 결제 하는 로직

---
+++ src/main/java/shop/itbook/itbookshop/ordergroup/orderproduct/exception/OrderProductNotFoundException.java
@@ -0,0 +1,7 @@
+package shop.itbook.itbookshop.ordergroup.orderproduct.exception;/**
+ * 
+ * 
+ * @author 정재원 
+ * @since 1.0
+ */public class OrderProductException {
+}
---
+++ src/main/java/shop/itbook/itbookshop/ordergroup/orderproduct/service/impl/OrderProductServiceImpl.java
@@ -0,0 +1,7 @@
+package shop.itbook.itbookshop.ordergroup.orderproduct.service.impl;/**
+ * 
+ * 
+ * @author 정재원 
+ * @since 1.0
+ */public class OrderProductServiceImpl {
+}
