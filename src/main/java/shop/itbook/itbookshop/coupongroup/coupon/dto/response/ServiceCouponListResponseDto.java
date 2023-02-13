c229abe1ee38f70e695f221d67783bc553362995
dahye <sdh46@chosun.kr>
2023-02-14 01:28:09 +0900

    [feat] : 엘라스틱 서치 오류 수정 멤버십 쿠폰 조회 생성

--- src/main/java/shop/itbook/itbookshop/coupongroup/coupon/dto/response/ServiceCouponListResponseDto.java
+++ src/main/java/shop/itbook/itbookshop/coupongroup/coupon/dto/response/ServiceCouponListResponseDto.java
@@ -1,8 +1,30 @@
 package shop.itbook.itbookshop.coupongroup.coupon.dto.response;
 
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 /**
  * @author 송다혜
  * @since 1.0
  */
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ServiceCouponListResponseDto {
+    private Long couponNo;
+    private String couponType;
+    private String name;
+    private String code;
+    private Long amount;
+    private Integer percent;
+    private Long point;
+    private LocalDateTime couponCreatedAt;
+    private LocalDateTime couponExpiredAt;
+    private Integer categoryNo;
+    private String categoryName;
+    private String parentCategoryName;
+    private Long productNo;
+    private String productName;
 }
