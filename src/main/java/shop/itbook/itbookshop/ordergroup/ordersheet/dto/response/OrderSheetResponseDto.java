6b677bff39ae5275ec018997ab3cd56d13d25b3c
HeLLo2 <popt0@naver.com>
2023-02-17 03:39:34 +0900

    [refactor] : 일반 상품, 구독 상품 주문서 리팩토링

--- src/main/java/shop/itbook/itbookshop/ordergroup/ordersheet/dto/response/OrderSheetResponseDto.java
+++ src/main/java/shop/itbook/itbookshop/ordergroup/ordersheet/dto/response/OrderSheetResponseDto.java
@@ -1,15 +1,13 @@
 package shop.itbook.itbookshop.ordergroup.ordersheet.dto.response;
 
 import java.util.List;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 import shop.itbook.itbookshop.membergroup.memberdestination.dto.response.MemberDestinationResponseDto;
 import shop.itbook.itbookshop.productgroup.product.dto.response.ProductDetailsResponseDto;
 
 /**
- * 주문서 작성 시 필요한 정보를 반환하는 Dto
+ * 주문서 작성 시 필요한 정보를 반환하는 Dto.
  *
  * @author 정재원
  * @since 1.0
@@ -19,9 +17,17 @@ public class OrderSheetResponseDto {
     private final List<ProductDetailsResponseDto> productDetailsResponseDtoList;
     private final List<MemberDestinationResponseDto> memberDestinationResponseDtoList;
 
-    private Long deliveryFee;
-    private Long memberPoint;
+    private final Long deliveryFee;
+    private final Long memberPoint;
 
+    /**
+     * 주문서 Dto 생성자.
+     *
+     * @param productDetailsResponseDtoList    제품 상세 정보 Dto 리스트
+     * @param memberDestinationResponseDtoList 회원 배송지 정보 Dto 리스트
+     * @param deliveryFee                      배송비
+     * @param memberPoint                      회원이 가지고 있는 포인트
+     */
     @Builder
     public OrderSheetResponseDto(List<ProductDetailsResponseDto> productDetailsResponseDtoList,
                                  List<MemberDestinationResponseDto> memberDestinationResponseDtoList,
