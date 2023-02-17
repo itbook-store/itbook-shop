d349b00b1e481b5cd6f6934cb464fce5a4735a1c
jun1718 <gbeovhsqhtka@naver.com>
2023-02-17 14:42:19 +0900

    [fix] : 테스트 오류

--- src/test/java/shop/itbook/itbookshop/deliverygroup/delivery/service/serviceapi/DeliveryServiceImplTest.java
+++ src/test/java/shop/itbook/itbookshop/deliverygroup/delivery/service/serviceapi/DeliveryServiceImplTest.java
@@ -53,8 +53,6 @@ public class DeliveryServiceImplTest {
 
     @MockBean
     OrderStatusHistoryService orderStatusHistoryService;
-    @MockBean
-    OrderRepository orderRepository;
 
     Order order;
 
