215074626f2f76543729c79cbce9fd57b96e577c
jun1718 <gbeovhsqhtka@naver.com>
2023-02-20 01:11:10 +0900

    [feat] : 리뷰차감 포인트히스토리 생성 메서드 추가

--- src/main/java/shop/itbook/itbookshop/pointgroup/pointhistorychild/review/service/ReviewIncreasePointHistoryService.java
+++ src/main/java/shop/itbook/itbookshop/pointgroup/pointhistorychild/review/service/ReviewIncreasePointHistoryService.java
@@ -1,5 +1,6 @@
 package shop.itbook.itbookshop.pointgroup.pointhistorychild.review.service;
 
+import org.springframework.transaction.annotation.Transactional;
 import shop.itbook.itbookshop.membergroup.member.entity.Member;
 import shop.itbook.itbookshop.pointgroup.pointhistorychild.review.entity.ReviewIncreasePointHistory;
 import shop.itbook.itbookshop.productgroup.review.entity.Review;
@@ -10,6 +11,10 @@ import shop.itbook.itbookshop.productgroup.review.entity.Review;
  */
 public interface ReviewIncreasePointHistoryService {
     ReviewIncreasePointHistory savePointHistoryAboutReviewIncrease(Member member,
+                                                                   Review review,
+                                                                   Long pointToApply);
+
+    ReviewIncreasePointHistory savePointHistoryAboutReviewDecrease(Member member,
                                                                    Review review,
                                                                    Long pointToApply);
 }
