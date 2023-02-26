a696aa2f769a9bf4cf5ae043ee235326ccf5cda4
suy00065 <suy00065@gmail.com>
2023-02-26 12:34:12 +0900

    [test] : deliveryService 테스트 코드 작성

--- src/test/java/shop/itbook/itbookshop/deliverygroup/delivery/service/serviceapi/DeliveryServiceImplTest.java
+++ src/test/java/shop/itbook/itbookshop/deliverygroup/delivery/service/serviceapi/DeliveryServiceImplTest.java
@@ -1,10 +1,11 @@
 package shop.itbook.itbookshop.deliverygroup.delivery.service.serviceapi;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 
 import java.util.Optional;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -27,6 +28,12 @@ import shop.itbook.itbookshop.deliverygroup.deliverystatushistory.repository.Del
 import shop.itbook.itbookshop.ordergroup.order.dummy.OrderDummy;
 import shop.itbook.itbookshop.ordergroup.order.entity.Order;
 import shop.itbook.itbookshop.ordergroup.order.repository.OrderRepository;
+import shop.itbook.itbookshop.ordergroup.orderstatus.dummy.OrderStatusDummy;
+import shop.itbook.itbookshop.ordergroup.orderstatus.entity.OrderStatus;
+import shop.itbook.itbookshop.ordergroup.orderstatus.repository.OrderStatusRepository;
+import shop.itbook.itbookshop.ordergroup.orderstatusenum.OrderStatusEnum;
+import shop.itbook.itbookshop.ordergroup.orderstatushistory.entity.OrderStatusHistory;
+import shop.itbook.itbookshop.ordergroup.orderstatushistory.repository.OrderStatusHistoryRepository;
 import shop.itbook.itbookshop.ordergroup.orderstatushistory.service.OrderStatusHistoryService;
 
 /**
@@ -54,6 +61,12 @@ public class DeliveryServiceImplTest {
     @MockBean
     OrderStatusHistoryService orderStatusHistoryService;
 
+    @MockBean
+    OrderStatusHistoryRepository orderStatusHistoryRepository;
+
+    @MockBean
+    OrderStatusRepository orderStatusRepository;
+
     Order order;
 
     Delivery delivery;
@@ -61,6 +74,10 @@ public class DeliveryServiceImplTest {
     DeliveryStatus deliveryStatus;
 
     DeliveryStatusHistory deliveryStatusHistory;
+
+    OrderStatusHistory dummyOrderStatusHistory;
+
+    OrderStatus dummyOrderStatus;
 
 
     @BeforeEach
@@ -70,8 +87,7 @@ public class DeliveryServiceImplTest {
         delivery = DeliveryDummy.createDelivery(order);
         deliveryStatus = DeliveryStatusDummy.getDummyWait();
         deliveryStatusHistory = DeliveryStatusHistoryDummy.getDeliveryStatusHistory();
-
-
+        dummyOrderStatus = OrderStatusDummy.createByEnum(OrderStatusEnum.DELIVERY_COMPLETED);
     }
 
     @DisplayName("registerDelivery 배송 등록 && 주문의 상태이력 배송대기 상태로 추가 테스트")
@@ -95,5 +111,61 @@ public class DeliveryServiceImplTest {
         assertThat(deliveryStatusHistory.getDeliveryStatus().getDeliveryStatusEnum())
             .isEqualTo(deliveryStatus.getDeliveryStatusEnum());
 
+        deliveryService.registerDelivery(order);
+
+    }
+
+    @Test
+    void findTrackingNoByOrderNo() {
+        given(deliveryRepository.save(delivery)).willReturn(delivery);
+        given(deliveryStatusRepository.findByDeliveryStatusEnum(DeliveryStatusEnum.WAIT_DELIVERY))
+            .willReturn(Optional.of(deliveryStatus));
+
+        deliveryStatusHistory.setDelivery(delivery);
+
+        given(deliveryRepository.findTrackingNoByOrderNo(anyLong())).willReturn(
+            delivery.getTrackingNo());
+
+        assertThat(deliveryService.findTrackingNoByOrderNo(order.getOrderNo())).isEqualTo(
+            delivery.getDeliveryNo());
+    }
+
+    @Test
+    void completeDelivery() {
+        deliveryStatusHistory = DeliveryStatusHistoryDummy.getDeliveryStatusHistory2();
+
+        // given
+        given(deliveryRepository.save(delivery)).willReturn(delivery);
+        given(deliveryRepository.findById(delivery.getDeliveryNo())).willReturn(
+            Optional.ofNullable(delivery));
+
+        Delivery testDelivery = deliveryRepository.findById(delivery.getDeliveryNo()).orElseThrow();
+
+        deliveryStatus = new DeliveryStatus(2, DeliveryStatusEnum.DELIVERY_COMPLETED);
+        given(deliveryStatusRepository.save(deliveryStatus)).willReturn(deliveryStatus);
+        given(deliveryStatusRepository.findByDeliveryStatusEnum(
+            deliveryStatus.getDeliveryStatusEnum()))
+            .willReturn(Optional.of(deliveryStatus));
+
+        given(orderRepository.save(order)).willReturn(order);
+        given(orderRepository.findOrderByDeliveryNo(any())).willReturn(order);
+        Order testOrder = orderRepository.findOrderByDeliveryNo(testDelivery.getDeliveryNo());
+        testDelivery.setOrder(testOrder);
+
+        deliveryStatusHistory.setDelivery(testDelivery);
+
+
+        // when
+        given(deliveryStatusHistoryRepository.save(deliveryStatusHistory))
+            .willReturn(deliveryStatusHistory);
+
+        // then
+        assertThat(deliveryStatusHistory.getDelivery().getDeliveryNo())
+            .isEqualTo(testDelivery.getDeliveryNo());
+
+        assertThat(deliveryStatusHistory.getDeliveryStatus().getDeliveryStatusEnum())
+            .isEqualTo(deliveryStatus.getDeliveryStatusEnum());
+
+        deliveryService.completeDelivery(testDelivery.getDeliveryNo());
     }
 }
